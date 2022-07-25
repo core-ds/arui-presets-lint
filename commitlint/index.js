006ca95428762c9299473fa2ba5bd7d7f03e1ccb
Aleksandr Kitov <kitov.nn@gmail.com>
2022-07-25 10:57:24 +0300

    feat(commitlint): increase body-max-line-length

--- commitlint/index.js
+++ commitlint/index.js
@@ -2,5 +2,6 @@ module.exports = {
     extends: '@commitlint/config-conventional',
     rules: {
         'scope-empty': [2, 'never'],
+        'body-max-line-length': [2, 'always', 200],
     },
 };
