49f9c06be2ba5cdbde27ee4226a86834fd573fb5
Kirill Skvortsov <kiskv@outlook.com>
2023-12-10 02:06:57 +0300

    feat(*): modify tests

--- test/cn.js
+++
@@ -1,3 +0,0 @@
-export function cn(target) {
-    return target;
-}
--- test/js-input.jsx
+++ test/js-input.jsx
@@ -1,49 +1,10 @@
-/* eslint-disable no-console */
 import React from 'react';
-import babelCore from 'stylelint';
-
-import { cn } from './cn';
-
-const someArray = [
-    'hello',
-    'this',
-    'is',
-    'some',
-    'array',
-    'and',
-    'it',
-    'has',
-    'many',
-    'items',
-    'that',
-    'should',
-    'be',
-    'wrapped',
-];
-
-@cn('answer-giver')
-class AnswersGiver {
-    question = 'The answer to the ultimate question of life, the universe and everything is';
-
-    getAnswer() {
-        return `${this.question} 42`;
-    }
-}
-
-const giver = new AnswersGiver();
 
 definedAfterUsage();
 
-console.log(giver.getAnswer(), someArray);
-console.log(babelCore.version);
-
 function definedAfterUsage() {
+    // eslint-disable-next-line no-console
     console.log('Because it is normal!');
 }
 
 export const element = <div style={{ color: 'black' }} />;
-
-const it = () => {};
-
-// eslint-disable-next-line no-restricted-properties
-it.only('this is a test');
