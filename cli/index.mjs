118946f784422d01b8ef6d72c13bb9f4b878b484
Kirill Skvortsov <kiskv@outlook.com>
2024-09-24 19:14:36 +0300

    feat(*): add lefthook and cli, remove simple-git-hooks and lint-staged

---
+++ cli/index.mjs
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import { execaCommand } from 'execa';
+
+const prettierParams =
+    '"./**/*.{ts,tsx,js,jsx,mjs,mts,cjs,cts,css,json}" --no-error-on-unmatched-pattern --cache';
+const cacheFolder = './node_modules/.cache';
+
+const commandsMap = {
+    styles: `stylelint "**/*.css" --allow-empty-input --ignore-path .gitignore --ignore-path .stylelintignore --cache --cache-location="${cacheFolder}/stylelint/.stylelintcache"`,
+    scripts: `eslint "**/*.{js,jsx,ts,tsx,mjs,mts,cjs,cts}" --ext .js,.jsx,.ts,.tsx,.mjs,.mts,.cjs,.cts --ignore-pattern=.gitignore,.eslintignore --cache --cache-location="${cacheFolder}/eslint/.eslintcache"`,
+    format: `prettier --write ${prettierParams}`,
+    'format:check': `prettier --check ${prettierParams}`,
+};
+
+const commands = Object.keys(commandsMap);
+const enableEcho = process.argv[2] === '--echo';
+const command = enableEcho ? process.argv[3] : process.argv[2];
+
+if (!command || !commands.includes(command)) {
+    // eslint-disable-next-line no-console
+    console.error(`Please specify one of available commands: ${commands.join(' ')}`);
+
+    process.exit(-1);
+}
+
+const args = enableEcho ? process.argv.slice(4) : process.argv.slice(3);
+
+const exec = [commandsMap[command], ...args].join(' ');
+
+if (enableEcho) {
+    // eslint-disable-next-line no-console
+    console.log('>>', exec);
+}
+
+try {
+    await execaCommand(exec, {
+        shell: true,
+        preferLocal: true,
+        stdio: ['pipe', 'inherit', 'inherit'],
+    });
+} catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+    process.exit(error.exitCode);
+}
