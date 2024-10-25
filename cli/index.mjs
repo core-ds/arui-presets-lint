e562839ae0cf4b91503ed5c4cf65e5346c035358
Kirill Skvortsov <kiskv@outlook.com>
2024-10-25 23:57:54 +0300

    feat(*): add debug to cli

--- cli/index.mjs
+++ cli/index.mjs
@@ -4,26 +4,34 @@ import { execaCommand } from 'execa';
 
 const prettierParams =
     '"./**/*.{ts,tsx,js,jsx,mjs,mts,cjs,cts,css,json}" --no-error-on-unmatched-pattern --cache';
+const cacheFolder = './node_modules/.cache';
 
 const commandsMap = {
-    styles: 'stylelint "**/*.css" --allow-empty-input --ignore-path .gitignore --ignore-path .stylelintignore --cache --cache-location="./node_modules/.cache/stylelint/.stylelintcache"',
-    scripts:
-        'eslint "**/*.{js,jsx,ts,tsx,mjs,mts,cjs,cts}" --ext .js,.jsx,.ts,.tsx,.mjs,.mts,.cjs,.cts --ignore-pattern=.gitignore,.eslintignore --cache --cache-location="./node_modules/.cache/eslint/.eslintcache"',
+    styles: `stylelint "**/*.css" --allow-empty-input --ignore-path .gitignore --ignore-path .stylelintignore --cache --cache-location="${cacheFolder}/stylelint/.stylelintcache"`,
+    scripts: `eslint "**/*.{js,jsx,ts,tsx,mjs,mts,cjs,cts}" --ext .js,.jsx,.ts,.tsx,.mjs,.mts,.cjs,.cts --ignore-pattern=.gitignore,.eslintignore --cache --cache-location="${cacheFolder}/eslint/.eslintcache"`,
     format: `prettier --write ${prettierParams}`,
     'format:check': `prettier --check ${prettierParams}`,
 };
 
 const commands = Object.keys(commandsMap);
-const command = process.argv[2];
-const args = process.argv.slice(3);
+const enableEcho = process.argv[2] === '--echo';
+const command = enableEcho ? process.argv[3] : process.argv[2];
 
-const exec =
-    !command || !commands.includes(command)
-        ? `${command} ${args.join(' ')}`
-        : [commandsMap[command], ...args].join(' ');
+if (!command || !commands.includes(command)) {
+    // eslint-disable-next-line no-console
+    console.error(`Please specify one of available commands: ${commands.join(' ')}`);
 
-// eslint-disable-next-line no-console
-console.log(exec);
+    process.exit(-1);
+}
+
+const args = enableEcho ? process.argv.slice(4) : process.argv.slice(3);
+
+const exec = [commandsMap[command], ...args].join(' ');
+
+if (enableEcho) {
+    // eslint-disable-next-line no-console
+    console.log('>>', exec);
+}
 
 try {
     await execaCommand(exec, {
