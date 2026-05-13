3d48c72d4cfb1aa2fd5d46a1eab76d7aef2fb453
Kirill Skvortsov <kiskv@outlook.com>
2026-05-13 18:16:17 +0300

    chore(arui-presets-lint): change variable

--- packages/arui-presets-lint/eslint/constants.ts
+++ packages/arui-presets-lint/eslint/constants.ts
@@ -1,12 +1,12 @@
-const scriptsExtentionsPattern = 'js,mjs,cjs,jsx,mjsx,ts,tsx,mts,cts,mtsx,ctsx';
+const scriptsExtentionsPattern = '{js,mjs,cjs,jsx,mjsx,ts,tsx,mts,cts,mtsx,ctsx}';
 
-export const GLOBAL_SCRIPTS_SCOPE = `**/*.{${scriptsExtentionsPattern}}`;
-export const NODEJS_SCRIPTS_SCOPE = `**/server/**/*.{${scriptsExtentionsPattern}}`;
+export const GLOBAL_SCRIPTS_SCOPE = `**/*.${scriptsExtentionsPattern}`;
+export const NODEJS_SCRIPTS_SCOPE = `**/server/**/*.${scriptsExtentionsPattern}`;
 export const TYPESCRIPT_SCRIPTS_SCOPE = '**/*.{ts,tsx,mts,cts,mtsx,ctsx}';
 export const REACT_SCRIPTS_SCOPE = '**/*.{jsx,mjsx,tsx,mtsx,ctsx}';
-export const TESTS_SCRIPTS_SCOPE = `**/*.{test,tests,spec}.{${scriptsExtentionsPattern}}`;
-export const CYPRESS_SCOPE = `cypress/**/*.{${scriptsExtentionsPattern}}`;
-export const PLAYWRIGHT_SCOPE = `playwright/**/*.{${scriptsExtentionsPattern}}`;
+export const TESTS_SCRIPTS_SCOPE = `**/*.{test,tests,spec}.${scriptsExtentionsPattern}`;
+export const CYPRESS_SCOPE = `cypress/**/*.${scriptsExtentionsPattern}`;
+export const PLAYWRIGHT_SCOPE = `playwright/**/*.${scriptsExtentionsPattern}`;
 export const MARKDOWN_SCOPE = '**/*.md';
 export const JSON_SCOPE = '**/*.{json,json5,jsonc}';
 // Не должен пересекаться с остальными
